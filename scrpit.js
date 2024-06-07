d4df8c3dc75f3aaa5d07cac1fbec99aaee127ce8
Rohith P <rohithinteractive14@gmail.com>
2024-06-08 00:00:11 +0530

    favicon changed

---
+++ scrpit.js
@@ -0,0 +1,10 @@
+const menu = document.getElementById("menu");
+
+function mobilemenu(){
+    if(menu.style.display === "block"){
+        menu.style.display = "none";
+    }
+    else{
+        menu.style.display="block"
+    }
+}
